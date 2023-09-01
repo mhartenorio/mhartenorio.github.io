ebfecb1cd402c0b7532a7dde53cf3074f7c246ef
mhartenorio <mhartenorio10@gmail.com>
2023-09-01 14:02:20 -0700

    Add animations

--- src/pages/landing-v2.jsx
+++ src/pages/landing-v2.jsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from '@mui/material';
+import { Box, Container, Stack, Typography } from '@mui/material';
 import React from 'react';
 import Header from '../components/header';
 import HeaderV3 from '../components/header3';
@@ -7,6 +7,14 @@ import { DarkMode, LightMode } from '@mui/icons-material';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import image1 from '../images/Mhar.png';
+import image2 from '../images/Mhar2.JPG';
+import image3 from '../images/Mhar3.png';
+import image4 from '../images/Mhar4.png';
+import image5 from '../images/Mhar5.JPG';
+import image6 from '../images/Mhar6.JPG';
+import image7 from '../images/Mhar7.png';
+
 
 const GREEN = '#B0EE00'
 const ORANGE = '#EE4700'
@@ -20,11 +28,36 @@ const FONT_SIZE = {
   md: '72px'
 }
 
+const IMAGE_SIZE = {
+  xs: '50px',
+  sm: '68px',
+  md: '80px'
+}
+
+const IMAGES_SLIDESHOW = [image1, image2, image3, image4, image5, image6, image7]
+
 // const FONT_FAMILY = "'Overpass Mono', monospace"
 const FONT_FAMILY = 'Petrona'
 
 const LandingV2 = ({ isLightMode, textColor }) => {
-  
+  // const [waveClassName, setWaveClassName] = useState('wave');
+  const [waveHover, setWaveHover] = useState(false);
+  const [showMhar, setShowMhar] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // useEffect(() => {
+  //   if (!showMhar) return;
+    // const intervalId = setInterval(() => {
+    //   console.log(currentIndex)
+    //   if (currentIndex === IMAGES_SLIDESHOW.length - 1) {
+    //     setCurrentIndex(0);
+    //   }
+    //   else {
+    //     setCurrentIndex(currentIndex + 1);
+    //   }
+    //   return () => clearInterval(intervalId);
+    // }, 1000)
+  // }, [showMhar])
 
   return (
     <>
@@ -51,39 +84,64 @@ const LandingV2 = ({ isLightMode, textColor }) => {
           </Typography>
         </Marquee>
         <Stack rowGap={0} >
-          <Stack direction='row' columnGap={3} >
+          <Stack direction='row' columnGap={2} >
             <div style={{ width: '100%', height: '85%', background: GREEN, borderRadius: 200 }} />
-            <Typography sx={{
-              fontFamily: FONT_FAMILY,
-              whiteSpace: 'nowrap',
-              alignItems: 'center',
-              height: '100%',
-              fontSize: FONT_SIZE,
-              fontWeight: 300,
-              letterSpacing: -2,
-              color: textColor,
-              // fontStyle: 'italic',
-            }}>
-              Hello! 👋 I'm
+            <Typography
+              sx={{
+                fontFamily: FONT_FAMILY,
+                whiteSpace: 'nowrap',
+                alignItems: 'center',
+                height: '100%',
+                fontSize: FONT_SIZE,
+                fontWeight: 300,
+                letterSpacing: -2,
+                color: textColor,
+                // fontStyle: 'italic',
+              }}
+              // onMouseEnter={() => setWaveClassName('')}
+              // onMouseLeave={() => setWaveClassName('wave')}
+              onMouseEnter={() => setWaveHover(true)}
+              onMouseLeave={() => setWaveHover(false)}
+            >
+              Hello! <span class={'wave'}>{waveHover ? '🤝' : '👋'}</span> I'm
             </Typography>
           </Stack>
-          <Stack direction='row' columnGap={3}>
-            <Typography sx={{
-              fontFamily: FONT_FAMILY,
-              whiteSpace: 'nowrap',
-              alignItems: 'center',
-              height: '100%',
-              fontSize: FONT_SIZE,
-              // fontWeight: 600,
-              letterSpacing: -2,
-              color: textColor,
-            }}
+          <Stack direction='row' columnGap={2}>
+            <Typography
+              sx={{
+                fontFamily: FONT_FAMILY,
+                whiteSpace: 'nowrap',
+                alignItems: 'center',
+                height: '100%',
+                fontSize: FONT_SIZE,
+                // fontWeight: 600,
+                letterSpacing: -2,
+                color: textColor,
+                alignItems: 'center'
+              }}
+              onMouseEnter={() => setShowMhar(true)}
+              onMouseLeave={() => setShowMhar(false)}
             >
-              Mhar 👨‍💻 Tenorio
+              Mhar&nbsp;
+              {!showMhar ? (
+                <Box
+                  component="img"
+                  sx={{ width: IMAGE_SIZE, height: IMAGE_SIZE, display: 'inline-block', mb: -1, objectFit: 'cover' }}
+                  src={image1}
+                />
+              ) : (
+                // <Box
+                //   component="img"
+                //   sx={{ width: IMAGE_SIZE, height: IMAGE_SIZE, display: 'inline-block', mb: -1, objectFit: 'cover' }}
+                //   src={image1}
+                // />
+                <span>👨‍💻</span>
+              )}
+              &nbsp;Tenorio
             </Typography>
             <div style={{ width: '100%', height: '85%', background: ORANGE, borderRadius: 200 }} />
           </Stack>
-          <Stack direction='row' columnGap={3}>
+          <Stack direction='row' columnGap={2}>
             <div style={{ width: '100%', height: '85%', background: YELLOW, borderRadius: 200 }} />
             <Typography className='tab' sx={{
               fontFamily: FONT_FAMILY,
@@ -98,7 +156,7 @@ const LandingV2 = ({ isLightMode, textColor }) => {
               Experience 💻
             </Typography>
           </Stack>
-          <Stack direction='row' columnGap={3}>
+          <Stack direction='row' columnGap={2}>
             <Link to='/'>
               <Typography className='tab' sx={{
                 fontFamily: FONT_FAMILY,
@@ -115,7 +173,7 @@ const LandingV2 = ({ isLightMode, textColor }) => {
             </Link>
             <div style={{ width: '100%', height: '85%', background: BLUE, borderRadius: 200 }} />
           </Stack>
-          <Stack direction='row' columnGap={3}>
+          <Stack direction='row' columnGap={2}>
             <div style={{ width: '100%', height: '85%', background: PINK, borderRadius: 200 }} />
             <Link to='/'>
               <Typography className='tab' sx={{
