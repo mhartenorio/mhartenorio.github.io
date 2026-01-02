f5b9b247d7bcf198ca57db4e3f5447ef9b226abb
mhartenorio <mhartenorio10@gmail.com>
2026-01-02 03:00:34 -0800

    add scroll icon on mobile

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,21 +2,30 @@ import './navbar.css';
 
 export const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="nav-item logo-cell">
-        <span>⦧O'0 mhar tenorio ᝰ.ᐟ</span>
-      </div>
-      <div className="nav-menu">
-        <a className="nav-item" href='https://www.linkedin.com/in/mhartenorio/' target='_blank'>
-          resume
-        </a>
-        <a className="nav-item" href='https://github.com/mhartenorio' target='_blank'>
-          tech projects
-        </a>
-        <a className="nav-item" href='https://mhar.squarespace.com' target='_blank'>
-          art &amp; design
-        </a>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="nav-item">
+          <span className="logo-cell">⦧O'0 mhar tenorio ᝰ.ᐟ</span>
+          <span className="mobile-scroll-text">
+           {"→))"}
+          </span>
+        </div>
+        <div className="nav-menu">
+          <a className="nav-item" href='https://www.linkedin.com/in/mhartenorio/' target='_blank'>
+            resume
+          </a>
+          <a className="nav-item" href='https://github.com/mhartenorio' target='_blank'>
+            tech projects
+          </a>
+          <a className="nav-item" href='https://mhar.squarespace.com' target='_blank'>
+            art &amp; design
+          </a>
+          <a className="nav-item" href='https://mhar.squarespace.com' target='_blank'>
+            about me
+          </a>
+        </div>
+      </nav>
+
+    </>
   )
 }
